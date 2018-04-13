6ae4d73ef22bb650d868f5baab2884480a0df514
teaearlgraycold <supacoofoo@gmail.com>
2018-04-12 23:20:19 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mtpo.Mixfile do
   def project do
     [
       app: :mtpo,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
